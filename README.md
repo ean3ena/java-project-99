ade34741247aa5d191c86fac5876a806e2b4b29c
ean3ena <ean3ena@yandex.ru>
2025-02-09 23:55:54 +0300

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-### Hexlet tests and linter status:
 [![Actions Status](https://github.com/ean3ena/java-project-99/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/ean3ena/java-project-99/actions)
+
+[![Maintainability](https://api.codeclimate.com/v1/badges/9359d5536fc5320214b4/maintainability)](https://codeclimate.com/github/ean3ena/java-project-99/maintainability)
+[![Test Coverage](https://api.codeclimate.com/v1/badges/9359d5536fc5320214b4/test_coverage)](https://codeclimate.com/github/ean3ena/java-project-99/test_coverage)
---
+++ build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+	application
+	id("checkstyle")
+	id("org.springframework.boot") version "3.4.2"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "hexlet.code"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(23)
+	}
+}
+
+application {
+	mainClass = "hexlet.code.AppApplication"
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	annotationProcessor("org.projectlombok:lombok")
+	compileOnly("org.projectlombok:lombok")
+
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.security:spring-security-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
