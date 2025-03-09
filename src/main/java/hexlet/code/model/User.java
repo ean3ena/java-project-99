45319f87b28d15953d2a8413a430a6b83d858180
ean3ena <ean3ena@yandex.ru>
2025-03-09 15:08:04 +0300

    add User model

---
+++ src/main/java/hexlet/code/model/User.java
@@ -0,0 +1,36 @@
+package hexlet.code.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String password;
+
+    private Date createdAt;
+
+    private Date updatedAt;
+}
