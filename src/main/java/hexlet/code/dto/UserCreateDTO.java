0ea7e816cf4fd81f2566d60ec2e7a42dca829917
ean3ena <ean3ena@yandex.ru>
2025-03-16 14:58:51 +0300

    add dto for user entity

---
+++ src/main/java/hexlet/code/dto/UserCreateDTO.java
@@ -0,0 +1,24 @@
+package hexlet.code.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserCreateDTO {
+
+    private String firstName;
+
+    private String lastName;
+
+    @Email
+    @NotNull
+    private String email;
+
+    @NotNull
+    @Size(min = 3)
+    private String password;
+}
