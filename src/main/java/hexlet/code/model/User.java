2806748a49d8980b1521fa457298d508c0d41e2b
ean3ena <ean3ena@yandex.ru>
2025-03-16 14:46:13 +0300

    add validation

--- src/main/java/hexlet/code/model/User.java
+++ src/main/java/hexlet/code/model/User.java
@@ -2,17 +2,25 @@ package hexlet.code.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.util.Date;
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "users")
+@EntityListeners(AuditingEntityListener.class)
 @Getter
 @Setter
 public class User {
@@ -25,12 +33,18 @@ public class User {
 
     private String lastName;
 
+    @Email
+    @NotNull
     @Column(unique = true, nullable = false)
     private String email;
 
-    private String password;
+    @NotNull
+    @Size(min = 3)
+    private String passwordDigest;
 
-    private Date createdAt;
+    @CreatedDate
+    private LocalDate createdAt;
 
-    private Date updatedAt;
+    @LastModifiedDate
+    private LocalDate updatedAt;
 }
