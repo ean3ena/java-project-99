114b12eccc37b5f950779b8cd2134d9136435236
ean3ena <ean3ena@yandex.ru>
2025-04-20 21:18:59 +0300

    add WelcomeControllerTest

---
+++ src/test/java/hexlet/code/controller/WelcomeControllerTest.java
@@ -0,0 +1,29 @@
+package hexlet.code.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class WelcomeControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void welcomeTest() throws Exception {
+        mvc.perform(get("/welcome").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("Welcome to Spring")));
+    }
+}
