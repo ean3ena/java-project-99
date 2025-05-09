73f62c3337d8ed5932b0095805e4799f21ecb364
ean3ena <ean3ena@yandex.ru>
2025-05-09 21:34:28 +0300

    add Task Service

---
+++ src/main/java/hexlet/code/service/TaskService.java
@@ -0,0 +1,60 @@
+package hexlet.code.service;
+
+import hexlet.code.dto.task.TaskCreateDTO;
+import hexlet.code.dto.task.TaskDTO;
+import hexlet.code.dto.task.TaskUpdateDTO;
+import hexlet.code.exception.ResourceNotFoundException;
+import hexlet.code.mapper.TaskMapper;
+import hexlet.code.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskService {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @Autowired
+    private TaskMapper taskMapper;
+
+    public ResponseEntity<List<TaskDTO>> getAll() {
+        var tasks = taskRepository.findAll();
+        var result =  tasks.stream()
+                .map(taskMapper::map)
+                .toList();
+        return ResponseEntity.ok()
+                .header("X-Total-Count", String.valueOf(tasks.size()))
+                .body(result);
+    }
+
+    public TaskDTO findById(Long id) {
+        var task = taskRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Task with id " + id + " not found"));
+        var dto = taskMapper.map(task);
+        return dto;
+    }
+
+    public TaskDTO create(TaskCreateDTO taskData) {
+        var task = taskMapper.map(taskData);
+        taskRepository.save(task);
+        var dto = taskMapper.map(task);
+        return dto;
+    }
+
+    public TaskDTO update(TaskUpdateDTO taskData, Long id) {
+        var task = taskRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Task with id " + id + " not found"));
+        taskMapper.update(task, taskData);
+        taskRepository.save(task);
+        var dto = taskMapper.map(task);
+        return dto;
+    }
+
+    public void delete(Long id) {
+        taskRepository.deleteById(id);
+    }
+}
